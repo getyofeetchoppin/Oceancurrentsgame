245a5e8fae3cf990ba5df8e70979c06a13eb7f1b
getyofeetchoppin <149441456+getyofeetchoppin@users.noreply.github.com>
2023-11-21 23:24:17 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,33 @@
-// Quiz questions and logic
-const quizContainer = document.getElementById('quiz-container');
+// Game logic goes here
 
-// Sample quiz questions
-const questions = [
-    {
-        question: "Which of the following is an example of a sea feature?",
-        options: ["Mountain", "Valley", "Trench", "Plateau"],
-        correctAnswer: "Trench"
-    },
-    {
-        question: "How does an ocean trench form?",
-        options: ["Volcanic activity", "Weathering", "Erosion", "Plate tectonics"],
-        correctAnswer: "Plate tectonics"
-    },
-    {
-        question: "What is the primary factor influencing ocean circulation?",
-        options: ["Wind", "Temperature", "Salinity", "Depth"],
-        correctAnswer: "Wind"
-    },
-    {
-        question: "How do changes in ocean temperature impact climate?",
-        options: ["No impact", "Warms the climate", "Cools the climate", "Induces rainfall"],
-        correctAnswer: "Warms the climate"
-    },
-    {
-        question: "What is the term for the pattern of ocean circulation that influences climate?",
-        options: ["Ocean waves", "Ocean currents", "Ocean tide", "Ocean vortex"],
-        correctAnswer: "Ocean currents"
-    }
-];
+// Sample game variables
+let score = 0;
 
-// Function to display quiz questions
-function displayQuiz() {
-    let quizHTML = "";
-    questions.forEach((q, index) => {
-        quizHTML += `<div class="question">${index + 1}. ${q.question}</div>`;
-        q.options.forEach((option, optionIndex) => {
-            quizHTML += `
-                <div>
-                    <input type="radio" name="question${index}" id="option${index}${optionIndex}" value="${option}">
-                    <label for="option${index}${optionIndex}">${option}</label>
-                </div>
-            `;
-        });
-    });
-    quizContainer.innerHTML = quizHTML;
+// Function to start the game
+function startGame() {
+    console.log("Game started!");
+
+    // Sample game logic: Increment the score
+    score++;
+    console.log("Score: " + score);
+
+    // Update the game display
+    updateGameDisplay();
 }
 
-// Call the function to display the quiz
-displayQuiz();
+// Function to update the game display
+function updateGameDisplay() {
+    // Sample: Display the score in the console
+    console.log("Updated display - Score: " + score);
+
+    // Update the HTML display
+    const scoreElement = document.getElementById("score");
+    if (scoreElement) {
+        scoreElement.textContent = "Score: " + score;
+    }
+}
+
+// Call the startGame function when the page is loaded
+document.addEventListener("DOMContentLoaded", function () {
+    startGame();
+});
